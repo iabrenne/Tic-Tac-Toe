88e7cbd5568ba171a06652accc2547ab7dece0fc
Irina Brennen <irina.brennen@twc.com>
2017-10-27 23:21:31 -0500

    Fixed issue with tic-tac-toe grid showing before user picked x or o when resetting game during winner announcement

--- js/global.js
+++ js/global.js
@@ -8,10 +8,6 @@ var numOfClickedButtons = 0;
 
 var intervalNextRound; 
 
-var roundInProgress = true;
-
-var gameOverCheckInProgress;
-
 var winningCombos = [
 
     [1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]
--- js/handleUI.js
+++ js/handleUI.js
@@ -21,30 +21,18 @@ var hideStartOver = () => $("#button-div").css("display","none");
 
 var communicateGameOver = (myWinningInfo) => {  
     
-    gameOverCheckInProgress = true;
-
-    var winningRowInterval = setTimeout(() => {
-
         myWinner = myWinningInfo.winner;
         reset();
         hideGrid();
         $("#announce-winner").css("display","block");
         $("#announce-winner>h2").text((myWinner == "tie") ? "Tie!" : myWinner + " won!");
-        console.log("Hello from setTimeOut")
-    
-
-    },1000);
-  
 
 
-    intervalNextRound = setTimeout(()=> {
-
+    intervalNextRound = setTimeout(()=> {        
         hideWinAnnounce();
-        console.log("hello from intervalNextRound");
         showGrid();
-        
-        gameOverCheckInProgress = false;
-        
+
+
     }, 5000);
 
     
@@ -67,16 +55,11 @@ var xOrOChoiceMade = (event) => {
 
 var reset = () => {
      
-        // if user clicked start over while winner announcement was displaying, clear out winner announcement.
-        // Also cancel the auto restart of the next round as user may want to change if they want to play as X or O
-
-        console.log("Hello from reset");
 
         hideWinAnnounce();  
 
         window.clearTimeout(intervalNextRound);
 
-        hideGrid();
 
         var buttons = $(".tic-tac-toe-btn");
     
@@ -96,6 +79,8 @@ var reset = () => {
 
     
 var startOver = () => {
+
+        userStartedOver = true;
     
         reset();
 
